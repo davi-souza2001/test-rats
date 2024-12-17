04b5e58c2bf5bec59f6ad598ce18ea00f1db8d05
Davi Souza <davisouza2001dv@gmail.com>
2024-12-17 13:18:17 -0300

    feat: add image png

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'expo-router'
 import { View, Text, TouchableOpacity, Image, StyleSheet, ImageBackground } from 'react-native'
 
 const styles = StyleSheet.create({
@@ -8,7 +7,7 @@ const styles = StyleSheet.create({
     },
 });
 
-export default function Index() {
+export default function Login() {
     return (
         <View className='bg-[#040415] h-screen py-16 px-5 flex flex-col items-center justify-center gap-32'>
             <View>
@@ -22,11 +21,9 @@ export default function Index() {
                 <Text className='text-[#7F7F7F] text-xl font-medium'>We need to work out safely</Text>
             </View>
             <View className='flex items-center justify-center w-full'>
-                <Link href="/login" asChild>
-                    <TouchableOpacity className='bg-[#FD4E2C] h-20 w-full rounded-2xl flex items-center justify-center'>
-                        <Text className='text-xl font-bold text-white'>Let's Walk</Text>
-                    </TouchableOpacity>
-                </Link>
+                <TouchableOpacity className='bg-[#FD4E2C] h-20 w-full rounded-2xl flex items-center justify-center'>
+                    <Text className='text-xl font-bold text-white'>Login with Google</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
