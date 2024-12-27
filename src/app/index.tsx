c7bed534ec4f35d5ca284451456f7359ef3d6fea
Davi Souza <davisouza2001dv@gmail.com>
2024-12-27 12:28:52 -0300

    feat: add iconh and general detail component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
+import { GeneralDetail } from '@/components/GeneralDetail'
+
+import CalendarIcon from '@expo/vector-icons/MaterialIcons';
 import { View, Text, Image } from 'react-native'
 
 export default function Index() {
     return (
-        <View className='bg-[#040415] h-screen py-10 px-5 flex flex-col items-start justify-start'>
+        <View className='bg-[#040415] h-screen py-16 px-5 flex flex-col items-start justify-start'>
             <View className='flex flex-row items-center justify-between w-full'>
-                <View className='flex flex-row items-center justify-start gap-2'>
+                <View className='flex flex-row items-center justify-start gap-4'>
                     <Image
                         style={{
                             width: 50,
@@ -17,18 +20,35 @@ export default function Index() {
                         <Text className='text-xl font-bold text-white'>Welcome to Test Rats</Text>
                     </View>
                 </View>
-                <Text className='text-white'>
-                    asdas
-                </Text>
+                <View className='flex items-center justify-center w-12 h-12 border-[0.3px] border-white rounded-full'>
+                    <CalendarIcon name="calendar-month" size={20} color="white" />
+                </View>
             </View>
             <View className='flex items-center justify-center w-full h-40 gap-2'>
                 <Text className='text-4xl font-bold tracking-[.1rem] text-white'>1884 Kcal</Text>
                 <Text className='text-[#77777c]'>Total Kilocalories</Text>
             </View>
-            <View className='flex flex-row items-center justify-around w-full h-20 gap-2'>
-                <View className='flex items-center justify-center w-full gap-2'>
-                    <Text className='text-2xl font-bold tracking-[.1rem] text-white'>7580 m</Text>
-                    <Text className='text-sm text-[#77777c]'>Distance</Text>
+            <View className='flex flex-row items-center justify-around w-full h-12 gap-2'>
+                <GeneralDetail title='7580 m' subTitle='Distance' />
+                <GeneralDetail title='9832' subTitle='Steps' />
+                <GeneralDetail title='1280' subTitle='Points' />
+            </View>
+            <View className='flex items-center justify-center w-full h-52'>
+                <View className='flex items-start justify-center gap-5 p-3 bg-green-400 shadow-md h-36 w-36 rounded-3xl shadow-green-400'>
+                    <Text>asd</Text>
+                    <View className='w-full'>
+                        <View className='flex flex-row items-center justify-start w-full gap-1'>
+                            <Text className='text-xl font-bold text-white'>
+                                628
+                            </Text>
+                            <Text className='text-lg font-semibold text-white'>
+                                kcal
+                            </Text>
+                        </View>
+                        <Text className='text-sm font-medium text-white opacity-80'>
+                            Dumbbell
+                        </Text>
+                    </View>
                 </View>
             </View>
         </View>
---
+++ src/components/GeneralDetail/GeneralDetail.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+type GeneralDetailProps = {
+    title: string
+    subTitle: string
+}
+
+export const GeneralDetail = ({ title, subTitle }: GeneralDetailProps) => {
+    return (
+        <View className='flex items-center justify-center w-32 gap-2'>
+            <Text className='text-2xl font-bold tracking-[.1rem] text-white'>{title}</Text>
+            <Text className='text-sm text-[#77777c]'>{subTitle}</Text>
+        </View>
+    )
+}
